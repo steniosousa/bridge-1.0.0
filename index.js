bf524e730be1a6bd4c1787d68cfd637638d45e1b
steniosousa <steniosousaf@gmail.com>
2024-03-21 00:23:53 -0300

    create cors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const { default: axios } = require("axios")
 const express = require("express")
-
+const cors = require('cors');
 const app = express()
 const port = 3000
-
+app.use(cors({
+    origin: "*", // Allow requests from all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow all HTTP methods
+    headers: ["Content-Type", "Authorization", "X-Requested-With"], // Allow all headers
+    credentials: true, // Allow cookies to be sent with requests
+}));
 app.get('/', (req, res) => {
     res.send('funcionando')
 })
